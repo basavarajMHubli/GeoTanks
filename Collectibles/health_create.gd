d653c4b6fec43b8d68361779f9f49f763c6b5737
Basavaraj M. H <basavarajmh3@gmail.com>
2023-12-25 15:55:29 +0530

    Proto: Removed objective singleton and integrated objective system into level script

--- Collectibles/health_create.gd
+++ Collectibles/health_create.gd
@@ -5,8 +5,6 @@ extends Area3D
 var player_interactor: Node3D = null
 
 @onready var help_text = $HelpText
-@onready var objective = $Objective
-
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
@@ -28,8 +26,5 @@ func _input(event):
 
 
 func destroy_health_crate():
-	# TODO: Check if objective artifacts can be decoupled entirely
-	if objective:
-		objective.mark_complete()
-
+	print("HealthCrate: Freeing")
 	queue_free()
--- Collectibles/shell_create.gd
+++ Collectibles/shell_create.gd
@@ -5,7 +5,6 @@ extends Area3D
 var player_interactor: Node3D = null
 
 @onready var help_text = $HelpText
-@onready var objective = $Objective
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
@@ -27,8 +26,5 @@ func _input(event):
 
 
 func destroy_shell_crate():
-	# TODO: Check if objective artifacts can be decoupled entirely
-	if objective:
-		objective.mark_complete()
-
+	print("ShellCrate: Freeing")
 	queue_free()
