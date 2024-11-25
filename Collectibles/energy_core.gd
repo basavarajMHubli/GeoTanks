64e8ca0162887405a9c4b8e0b6d23476a629c1c5
Basavaraj M. H. <basavarajmh3@gmail.com>
2024-11-25 22:43:16 +0530

    Proto: Added interaction for energy core

---
+++ Collectibles/energy_core.gd
@@ -0,0 +1,27 @@
+extends Area3D
+
+var player_interactor: Node3D = null
+
+@onready var help_text = $HelpText
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		help_text.visible = true		
+		player_interactor = body
+
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		help_text.visible = false
+		player_interactor = null
+
+
+func _input(event):
+	if player_interactor and event.is_action_pressed("interact"):
+		print("EnergyCore: Interacting")		
+		destroy_enery_core()
+
+
+func destroy_enery_core():
+	print("EnergyCore: Freeing")
+	queue_free()
