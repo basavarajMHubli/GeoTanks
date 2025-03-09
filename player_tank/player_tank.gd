85e8edc7c2808de1427f58e430cf825ff5c6642b
Basavaraj M. H. <basavarajmh3@gmail.com>
2025-03-09 23:24:16 +0530

    Proto: Initial airstrike functionality

--- player_tank/player_tank.gd
+++ player_tank/player_tank.gd
@@ -9,8 +9,8 @@ extends CharacterBody3D
 
 var target_velocity := Vector3.ZERO
 var shell_scene := preload("res://shells/shell.tscn")
-
 const RAY_LENGTH := 2000
+signal airstrike
 
 @onready var health_bar = $StatsSubViewport/HealthBar
 
@@ -23,9 +23,9 @@ func _ready():
 
 func _physics_process(delta: float):
 	var direction := Vector3.ZERO
-	
+
 	rotate_turret()
-	
+
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 	if Input.is_action_pressed("move_backward"):
@@ -34,15 +34,15 @@ func _physics_process(delta: float):
 		rotate_y(turn_speed * delta)
 	if Input.is_action_pressed("turn_right"):
 		rotate_y(-turn_speed * delta)
-	
+
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		
+
 	target_velocity.z = direction.z * move_speed
-	
+
 	if not is_on_floor():
 		target_velocity.y -= (fall_acceleration * delta)
-	
+
 	velocity = target_velocity.rotated(Vector3.UP, rotation.y) * move_speed * delta
 	move_and_slide()
 
@@ -50,6 +50,9 @@ func _physics_process(delta: float):
 func _process(_delta: float):
 	if Input.is_action_just_pressed("fire"):
 		fire_shell()
+
+	if Input.is_action_just_pressed("airstrike"):
+		call_airstrike()
 
 
 func fire_shell():
@@ -61,21 +64,25 @@ func fire_shell():
 		get_parent().add_child(shell)
 		UIManager.update_shells(shell_count)
 		shell.connect("camera_shake", $SpringArm3D/Camera3D._on_player_tank_camera_shake)
-	
+
 	print("Player: shell count " + str(shell_count))
+
+
+func call_airstrike():
+	emit_signal("airstrike")
 
 
 func rotate_turret():
 	var space_state := get_world_3d().direct_space_state
 	var mouse_pos := get_viewport().get_mouse_position()
-	
+
 	var ray_origin = $SpringArm3D/Camera3D.project_ray_origin(mouse_pos)
 	var ray_end = ray_origin + $SpringArm3D/Camera3D.project_ray_normal(mouse_pos) * RAY_LENGTH
-	
+
 	var query := PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
 	query.collide_with_areas = true
 	query.exclude = [self]
-	
+
 	var result := space_state.intersect_ray(query)
 	if not result.is_empty():
 		var look_pos = result.position
@@ -87,7 +94,7 @@ func shell_hit(damage_value, _hit_point):
 	print("Player: took damage " + str(damage_value))
 	cur_health -= damage_value
 	health_bar.value = cur_health
-	
+
 	if cur_health <= 0:
 		player_die()
 
@@ -104,7 +111,7 @@ func health_update(gain):
 	cur_health += gain
 	if cur_health > 100:
 		cur_health = 100
-	
+
 	health_bar.value = cur_health
 	print("Player: Health update to " + str(cur_health))
 
