688e25046b9d3453305bb1161e91fb4076ae1128
Basavaraj M. H. <basavarajmh3@gmail.com>
2025-03-15 16:51:53 +0530

    #22 Added collectible drops
    Isn't really a drop, spawns at random location

--- scenes/level_3.gd
+++ scenes/level_3.gd
@@ -15,11 +15,13 @@ var obj_remaining := objectives.size()
 var is_extraction_timer_expired
 
 @export var enemy: PackedScene = preload("res://enemies/enemy_tank.tscn")
+@export var health_crate: PackedScene = preload("res://Collectibles/health_crate.tscn")
+@export var shell_crate: PackedScene = preload("res://Collectibles/shell_crate.tscn")
 @onready var player_tank = $PlayerTank
-@onready var shell_crate = $NavigationRegion3D/ShellCrate
-@onready var health_crate = $NavigationRegion3D/HealthCrate
 @onready var extraction_timer_label: Label = $CanvasLayer/ExtractionTimerLabel
 @onready var extraction_timer: Timer = $ExtractionTimer
+@onready var enemy_spawn_path_location: PathFollow3D = $EnemySpawnPath/SpawnPathLocation
+@onready var collectible_spawn_path_location: PathFollow3D = $CollectibleSpawnPath/SpawnPathLocation
 
 
 func _ready():
@@ -53,14 +55,31 @@ func _on_extraction_timer_timeout():
 
 func _on_enemy_spawn_timer_timeout():
 	print("Level-3: Spawning enemy")
-	var spawn_loc := $SpawnPath/SpawnPathLocation
-	spawn_loc.progress_ratio = randf()
+	enemy_spawn_path_location.progress_ratio = randf()
 
 	var enemy_obj := enemy.instantiate()
-	enemy_obj.transform = spawn_loc.transform
+	enemy_obj.transform = enemy_spawn_path_location.transform
 	add_child(enemy_obj)
 
 
 func kill_all_enemies():
 	print("Level-3: Destroy all enemies")
 	get_tree().call_group("enemies", "shell_hit", 1000, global_position)
+
+
+func _on_collectible_spawn_timer_timeout() -> void:
+	print("Level-3: Spawning collectible")
+	collectible_spawn_path_location.progress_ratio = randf()
+
+	if collectible_spawn_path_location.progress_ratio >= 0.5:
+		print("Level-3: Spawning health crate")
+		var health_crate_obj = health_crate.instantiate()
+		add_child(health_crate_obj)
+		health_crate_obj.transform = collectible_spawn_path_location.transform
+		health_crate_obj.transform.origin.y += 1
+	else:
+		print("Level-3: Spawning shell crate")
+		var shell_crate_obj = shell_crate.instantiate()
+		add_child(shell_crate_obj)
+		shell_crate_obj.transform = collectible_spawn_path_location.transform
+		shell_crate_obj.transform.origin.y += 1
