ed3286b8f367ba4c606f2be1c62a74f1e78eb3b7
Basavaraj M. Hubli <basavarajmh3@gmail.com>
2025-03-16 23:17:52 +0530

    #16 Added placeholder for shell reload indicator (#33)

--- UI/UIManager.gd
+++ UI/UIManager.gd
@@ -1,10 +1,27 @@
 extends Control
 
+var reload_delay := 0.0
+var elapsed_delay := 0.0
+var show_reload_request := false
+
+
 @onready var obj_v_box_container = $CanvasLayer/ObjectiveVBoxContainer
 @onready var canvas_layer = $CanvasLayer
 @onready var shells_label = $CanvasLayer/ShellsLabel
 @onready var air_shells_label: Label = $CanvasLayer/AirShellsLabel
 @onready var airstrike_background: Sprite2D = $CanvasLayer/AirstrikeBackground
+@onready var shell_reload_progress_bar: TextureProgressBar = $CanvasLayer/ShellReloadProgressBar
+
+
+func _process(delta: float) -> void:
+	if show_reload_request:
+		elapsed_delay += delta
+		shell_reload_progress_bar.value = (elapsed_delay / reload_delay) * 100
+		if elapsed_delay >= reload_delay:
+			reload_delay = 0.0
+			elapsed_delay = 0.0
+			show_reload_request = false
+			shell_reload_progress_bar.value = 0.0
 
 
 func display_obj(obj_text):
@@ -37,6 +54,11 @@ func update_airstrike_count(call_count: int):
 	air_shells_label.text = str(call_count)
 	air_shells_label.modulate = Color.WHITE
 	airstrike_background.modulate = Color.WHITE
+
+
+func start_shell_reload(delay):
+	show_reload_request = true
+	reload_delay = delay
 
 
 func ui_visibility(state: bool):
