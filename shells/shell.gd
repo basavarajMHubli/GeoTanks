c1d1e8b7a3805b6d9e4cd07b420260f98edffa62
Basavaraj M. H. <basavarajmh3@gmail.com>
2025-05-18 23:48:18 +0530

    Art: WIP Add blast particles
    Also change window mode

--- shells/shell.gd
+++ shells/shell.gd
@@ -6,13 +6,16 @@ var SHELL_DAMAGE = 50
 const KILL_TIMER = 1
 var timer = 0
 var hit_someting = false
+var smoke_gpu_particles: GPUParticles3D
+var shell_mesh: MeshInstance3D
 
 signal camera_shake
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area3D.connect("body_entered", self.collided)
-
+	smoke_gpu_particles = $SmokeGPUParticles
+	shell_mesh = $ShellMesh
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -35,5 +38,17 @@ func collided(body):
 
 
 func destroy():
+	# Hide and disable shell
+	smoke_gpu_particles.visible = false
+	shell_mesh.visible = false
+	set_physics_process(false)
+
+	# Show blast particles
+	var blast_particles : GPUParticles3D = $BlastGPUParticles
+	blast_particles.global_transform = global_transform
+	blast_particles.visible = true
+	blast_particles.emitting = true
+
 	camera_shake.emit()
+	await get_tree().create_timer(5.0).timeout
 	queue_free()
